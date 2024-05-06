41a5e4112a64b14199d68d8ab434c9f2f6fd4bcc
JuliCode07 <JuliCode07@gmail.com>
2024-05-06 16:23:09 -0500

    first commit

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -13,52 +13,72 @@ export const playlists: Playlist[] = [
   {
     id: "1",
     albumId: 1,
-    title: "Party Animal - Remix",
+    title: "Perreo vol. 1",
     color: colors.yellow,
-    cover:
-      "https://mosaic.scdn.co/300/ab67616d00001e0254e5aec17a447066f52874fbab67616d00001e02614b5974ec6e23185462700bab67616d00001e028b5e63d878d4b18ed356f581ab67616d00001e02a66def7d649149464edc8f1e",
-    artists: ["Playlist · Juli"],
+    cover: "https://i2o.scdn.co/image/ab67706c0000cfa3acb5973641fa50de474dfbbe",
+    artists: ["Playlist · C E R O"],
   },
   {
     id: "2",
     albumId: 2,
-    title: "Lo-Fi Chill Session",
+    title: "Party animal",
     color: colors.green,
     cover:
-      "https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187",
-    artists: ["Kupla", "Blue Fox"],
+      "https://mosaic.scdn.co/300/ab67616d00001e0254e5aec17a447066f52874fbab67616d00001e02614b5974ec6e23185462700bab67616d00001e028b5e63d878d4b18ed356f581ab67616d00001e02a66def7d649149464edc8f1e",
+    artists: ["Playlist · Partyanimalcass"],
   },
   {
     id: "3",
     albumId: 3,
-    title: "Study Session",
+    title: "No salgo de casa",
     color: colors.rose,
-    cover: "https://f4.bcbits.com/img/a1435058381_65.jpg",
-    artists: ["Tenno", "xander", "Team Astro"],
+    cover:
+      "https://mosaic.scdn.co/300/ab67616d00001e0230828b2442a7494533670ee1ab67616d00001e0252ff352cd503ceddbaf1d533ab67616d00001e027ce7c833a19b3552c318f080ab67616d00001e02c09ae45aa7c410ca6d194ded",
+    artists: ["Playlist · Maicol"],
   },
   {
     id: "4",
     albumId: 4,
-    title: "Blue Note Study Time",
+    title: "Temazos",
     color: colors.blue,
-    cover: "https://f4.bcbits.com/img/a1962013209_16.jpg",
-    artists: ["Raimu", "Yasumu"],
+    cover: "https://i2o.scdn.co/image/ab67706c0000cfa3ca9fa18683fe10c3df31c076",
+    artists: ["Playlist · Isaag09"],
   },
   {
     id: "5",
     albumId: 5,
-    title: "Chau Saura Session",
+    title: "Arcangel Fronteo",
     color: colors.purple,
-    cover: "https://f4.bcbits.com/img/a2793859494_16.jpg",
-    artists: ["Chau Saura", "amies", "kyu"],
+    cover:
+      "https://image-cdn-fa.spotifycdn.com/image/ab67706c0000da84ba6e1611cd2fb63317ce83e7",
+    artists: ["Playlist · Esteban Zuluan"],
   },
   {
     id: "6",
     albumId: 6,
-    title: "Like a Necessity",
+    title: "Clasicos de Arcangel",
     color: colors.orange,
-    cover: "https://f4.bcbits.com/img/a0363730459_16.jpg",
-    artists: ["WFS", "Nadav Cohen"],
+    cover:
+      "https://mosaic.scdn.co/300/ab67616d00001e02172b684f5e6482f962b9a045ab67616d00001e02294ea58a8ac4ae598c46d0b0ab67616d00001e0275f95942c2a9d2f2aace6efaab67616d00001e02eeaaa69786db98d2bf2771eb",
+    artists: ["Playlist · Relncfd90"],
+  },
+  {
+    id: "7",
+    albumId: 7,
+    title: "Guacamaya Mortal",
+    color: colors.orange,
+    cover:
+      "https://mosaic.scdn.co/300/ab67616d00001e0221b96f489d2d5109598d9116ab67616d00001e0260bfb7ce133b198e93c885b9ab67616d00001e02715585069226e965890a14bbab67616d00001e02bb63c0c59d18804d81d414e6",
+    artists: ["Playlist · WestCOL"],
+  },
+  {
+    id: "8",
+    albumId: 8,
+    title: "SALVAJE MEDELLIN",
+    color: colors.orange,
+    cover:
+      "https://image-cdn-ak.spotifycdn.com/image/ab67706c0000da848f7357dee31de160aaba9cf0",
+    artists: ["WFS", "C E R O"],
   },
 ];
 
